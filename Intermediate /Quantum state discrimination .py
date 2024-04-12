ae5a0d80d516c6963dbd4a43fa3f96e41323e6c1
Isabelle Heuzé <144680414+iheuze@users.noreply.github.com>
2024-04-12 17:02:42 +0100

    Update Quantum state discrimination .py

--- Intermediate /Quantum state discrimination .py
+++ Intermediate /Quantum state discrimination .py
@@ -20,10 +20,18 @@ def maximal_probability(theta_1, theta_2, p_1, p_2):
     """
 
 
-    # Put your code here
+    phi_1 = np.array([np.cos(theta_1), np.sin(theta_1)])
+    phi_2 = np.array([np.cos(theta_2), np.sin(theta_2)])
+
+    dm_1 = np.outer(phi_1, phi_1)
+    dm_2 = np.outer(phi_2, phi_2)
+    
+    delta = p_1 * dm_1 - p_2 * dm_2
+    
+    eigenvals = np.linalg.eigvals(delta)
 
     # Return the highest probability of distinguishing the states
-
+    return 0.5 + 0.5 * np.sum(np.abs(eigenvals))
 
 # These functions are responsible for testing the solution.
 
