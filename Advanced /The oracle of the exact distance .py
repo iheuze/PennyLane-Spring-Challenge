edef8ae5c2aa0d55e82ed9e38fac0b873e405c4b
Isabelle Heuzé <144680414+iheuze@users.noreply.github.com>
2024-04-14 16:21:03 +0100

    Update The oracle of the exact distance .py

--- Advanced /The oracle of the exact distance .py
+++ Advanced /The oracle of the exact distance .py
@@ -22,9 +22,36 @@ def oracle_distance(d):
 
     """
 
-
     # Put your code here
+    for i, j, k in [(5, 2, 7), (4, 1, 6), (3, 0, 9)]:  
+        qml.CNOT(wires=[i, j])
+        qml.MultiControlledX(control_wires=[j, i], wires=k, control_values='11')
+        qml.CNOT(wires=[j, (6+j)])
+        qml.MultiControlledX(control_wires=[j, (6+j)], wires=k, control_values='01')
+        qml.CNOT(wires=[i, j])
 
+    qml.MultiControlledX(control_wires=[8, 9], wires=7, control_values='11')
+    qml.CNOT(wires=[9,6])
+    qml.MultiControlledX(control_wires=[9, 7, 8], wires=6, control_values='100')
+
+    # Adjustments based on value of 'd'
+    if d >= 4:
+        qml.PauliX(wires=6)
+        d -= 4
+        
+    if d >= 2:
+        qml.PauliX(wires=7)
+        d -= 2
+        
+    if d >= 1:
+        qml.PauliX(wires=8)
+
+    # Apply phase flips to qubits 6, 7, 8
+    for i in [6, 7, 8]:
+        qml.PauliX(wires=i)
+
+    # Apply CCZ gate
+    qml.CCZ(wires=[6, 7, 8])
 
 # These functions are responsible for testing the solution.
 wires_m = [0, 1, 2]
